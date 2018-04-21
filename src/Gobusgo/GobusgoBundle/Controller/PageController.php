045707ddc3bfd263422752c137811cd01a1be448
Dimas <web-money-00@mail.ru>
2018-04-21 22:08:00 +0300

    callback + some styles template

--- src/Gobusgo/GobusgoBundle/Controller/PageController.php
+++ src/Gobusgo/GobusgoBundle/Controller/PageController.php
@@ -3,10 +3,18 @@
 namespace Gobusgo\GobusgoBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Gobusgo\GobusgoBundle\Entity\Enquiry;
+use Gobusgo\GobusgoBundle\Form\EnquiryType;
+use Gobusgo\GobusgoBundle\Form\CallType;
+use Gobusgo\GobusgoBundle\Entity\Call;
+use Swift_SmtpTransport;
+use Swift_Mailer;
+use Swift_Message;
 
 class PageController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
         $seoDescription = 'Index';
         $seoPage = $this->container->get('sonata.seo.page');
@@ -16,7 +24,28 @@ class PageController extends Controller
             ->addMeta('property', 'og:description', $seoDescription)
         ;
 
-        return $this->render('@GobusgoGobusgo/Page/index.html.twig');
+        $callform = $this->Call($request);
+
+        return $this->render('@GobusgoGobusgo/Page/index.html.twig', array(
+        'callform' =>$callform->createView()
+        ));
+    }
+
+    public function confirmAction(Request $request)
+    {
+        $seoDescription = 'Index';
+        $seoPage = $this->container->get('sonata.seo.page');
+        $seoPage
+            ->setTitle('GObusGO')
+            ->addMeta('name', 'description', $seoDescription)
+            ->addMeta('property', 'og:description', $seoDescription)
+        ;
+
+        $callform = $this->Call($request);
+
+        return $this->render('@GobusgoGobusgo/Page/confirm.html.twig', array(
+            'callform' =>$callform->createView()
+        ));
     }
 
     public function catalogAction()
@@ -135,4 +164,71 @@ class PageController extends Controller
 
         return $this->render('@GobusgoGobusgo/Page/contacts.html.twig');
     }
+
+    public function Call($request)
+    {
+        $call = new Call();
+
+        $callform = $this->createForm(CallType::class, $call);
+
+        if ($request->isMethod($request::METHOD_POST)) {
+
+            $callform->handleRequest($request);
+            if ($callform->isValid()) {
+
+                $this->Caller($call);
+
+                $request->getSession()
+                    ->getFlashBag()
+                    ->add('success', 'Ваше сообщение успешно отправлено.');
+            }
+        }
+
+        return $callform;
+
+    }
+
+    public function Mailer($enquiry)
+    {
+        $mailer = $this->Transport();
+
+        // Create a message
+        $message = Swift_Message::newInstance('Форма обратной связи')
+            ->setFrom(array('seo-newline@mail.ru' => 'Обратный звонок'))
+            ->setTo($this->container->getParameter('blogger_blog.emails.contact_email'))
+            ->setBody($this->renderView('BloggerBlogBundle:Page:contactEmail.txt.twig', array('enquiry' => $enquiry)));
+        ;
+
+        // Send the message
+        $mailer->send($message);
+    }
+
+    public function Caller($call)
+    {
+        $mailer = $this->Transport();
+
+        // Create a message
+        $message = Swift_Message::newInstance('Форма обратного звонка')
+            ->setFrom(array('seo-newline@mail.ru' => 'Обратный звонок'))
+            ->setTo($this->container->getParameter('blogger_blog.emails.contact_email'))
+            ->setBody($this->renderView('@GobusgoGobusgo/Page/callEmail.txt.twig', array('call' => $call)));
+        ;
+
+        // Send the message
+        $mailer->send($message);
+    }
+
+    public function Transport()
+    {
+        // Create the Transport
+        $transport = Swift_SmtpTransport::newInstance('smtp.mail.ru', 465, 'ssl')
+            ->setUsername('seo-newline@mail.ru')
+            ->setPassword('19086837dima')
+        ;
+
+        // Create the Mailer using your created Transport
+        return $mailer = Swift_Mailer::newInstance($transport);
+
+    }
 }
+
---
+++ src/Gobusgo/GobusgoBundle/Form/CallType.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sambo
+ * Date: 21.04.2018
+ * Time: 19:57
+ */
+
+namespace Gobusgo\GobusgoBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+
+class CallType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name', TextType::class);
+        $builder->add('phone', NumberType::class);
+    }
+
+//    public function configureOptions(OptionsResolver $resolver)
+//    {
+//
+//    }
+
+//    public function getBlockPrefix()
+//    {
+//        return 'contact';
+//    }
+}
