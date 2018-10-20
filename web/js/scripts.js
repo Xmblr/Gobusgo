0c12baafcb8490357e4dbc8b6ae104f3415825e7
0dima0 <web-money-00@mail.ru>
2018-10-20 06:54:36 +0300

    styles

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -21,103 +21,103 @@ $(document).mouseup(function(e) {
     if ($target.closest(".accordion").length == 0) {
         $(".accordion").removeClass("active");
         $(".hamburger").removeClass("is-active");
-        e.style.transition = "all 5s ease-in-out";
+        e.style.transition = "all 1s ease-in-out";
     }
 });
 
+//
+// // Switcher Input
+//
+// function switcher() {
+//     a = $("#z").val();b = $("#k").val();$("#z").val(b);$("#k").val(a);
+// }
+//
+// // Text slide
+//
+// var timerlen = 5;
+// var slideAniLen = 250;
+//
+// var timerID = new Array();
+// var startTime = new Array();
+// var obj = new Array();
+// var endHeight = new Array();
+// var moving = new Array();
+// var dir = new Array();
 
-// Switcher Input
-
-function switcher() {
-    a = $("#z").val();b = $("#k").val();$("#z").val(b);$("#k").val(a);
-}
-
-// Text slide
-
-var timerlen = 5;
-var slideAniLen = 250;
-
-var timerID = new Array();
-var startTime = new Array();
-var obj = new Array();
-var endHeight = new Array();
-var moving = new Array();
-var dir = new Array();
-
-function slidedown(objname) {
-    if (moving[objname])
-        return;
-
-    if (document.getElementById(objname).style.display != "none")
-        return; // cannot slide down something that is already visible
-
-    moving[objname] = true;
-    dir[objname] = "down";
-    startslide(objname);
-}
-
-function slideup(objname) {
-    if (moving[objname])
-        return;
-
-    if (document.getElementById(objname).style.display == "none")
-        return; // cannot slide up something that is already hidden
-
-    moving[objname] = true;
-    dir[objname] = "up";
-    startslide(objname);
-}
-
-if (window.innerWidth < 468) {
-    function startslide(objname) {
-        obj[objname] = document.getElementById(objname);
-
-        endHeight[objname] = parseInt(obj[objname].style.height);
-        startTime[objname] = (new Date()).getTime();
-
-        if (dir[objname] == "down") {
-            obj[objname].style.height = "1px";
-        }
-
-        obj[objname].style.display = "block";
-
-        timerID[objname] = setInterval('slidetick(\'' + objname + '\');', timerlen);
-    }
-
-    function slidetick(objname) {
-        var elapsed = (new Date()).getTime() - startTime[objname];
-
-        if (elapsed > slideAniLen)
-            endSlide(objname)
-        else {
-            var d = Math.round(elapsed / slideAniLen * endHeight[objname]);
-            if (dir[objname] == "up")
-                d = endHeight[objname] - d;
-
-            obj[objname].style.height = d + "px";
-        }
-
-        return;
-    }
-
-    function endSlide(objname) {
-        clearInterval(timerID[objname]);
-
-        if (dir[objname] == "up")
-            obj[objname].style.display = "none";
-
-        obj[objname].style.height = endHeight[objname] + "px";
-
-        delete(moving[objname]);
-        delete(timerID[objname]);
-        delete(startTime[objname]);
-        delete(endHeight[objname]);
-        delete(obj[objname]);
-        delete(dir[objname]);
-
-        return;
-    }
-}
+// function slidedown(objname) {
+//     if (moving[objname])
+//         return;
+//
+//     if (document.getElementById(objname).style.display != "none")
+//         return; // cannot slide down something that is already visible
+//
+//     moving[objname] = true;
+//     dir[objname] = "down";
+//     startslide(objname);
+// }
+//
+// function slideup(objname) {
+//     if (moving[objname])
+//         return;
+//
+//     if (document.getElementById(objname).style.display == "none")
+//         return; // cannot slide up something that is already hidden
+//
+//     moving[objname] = true;
+//     dir[objname] = "up";
+//     startslide(objname);
+// }
+//
+// if (window.innerWidth < 468) {
+//     function startslide(objname) {
+//         obj[objname] = document.getElementById(objname);
+//
+//         endHeight[objname] = parseInt(obj[objname].style.height);
+//         startTime[objname] = (new Date()).getTime();
+//
+//         if (dir[objname] == "down") {
+//             obj[objname].style.height = "1px";
+//         }
+//
+//         obj[objname].style.display = "block";
+//
+//         timerID[objname] = setInterval('slidetick(\'' + objname + '\');', timerlen);
+//     }
+//
+//     function slidetick(objname) {
+//         var elapsed = (new Date()).getTime() - startTime[objname];
+//
+//         if (elapsed > slideAniLen)
+//             endSlide(objname)
+//         else {
+//             var d = Math.round(elapsed / slideAniLen * endHeight[objname]);
+//             if (dir[objname] == "up")
+//                 d = endHeight[objname] - d;
+//
+//             obj[objname].style.height = d + "px";
+//         }
+//
+//         return;
+//     }
+//
+//     function endSlide(objname) {
+//         clearInterval(timerID[objname]);
+//
+//         if (dir[objname] == "up")
+//             obj[objname].style.display = "none";
+//
+//         obj[objname].style.height = endHeight[objname] + "px";
+//
+//         delete(moving[objname]);
+//         delete(timerID[objname]);
+//         delete(startTime[objname]);
+//         delete(endHeight[objname]);
+//         delete(obj[objname]);
+//         delete(dir[objname]);
+//
+//         return;
+//     }
+// }
 
 // Hamburgers menu
 
@@ -1641,6 +1641,8 @@ $document.ready(function () {
             }
         }
     }
+
+
 
 
     /**
